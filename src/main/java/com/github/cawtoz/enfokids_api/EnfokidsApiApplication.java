d88f92ed0dff29e7d037d2c181fefe354bbd556d
cawtoz <cawtoz.dev@gmail.com>
2025-10-01 16:32:16 -0500

    setup project

---
+++ src/main/java/com/github/cawtoz/enfokids_api/EnfokidsApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.cawtoz.enfokids_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnfokidsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnfokidsApiApplication.class, args);
+	}
+
+}
