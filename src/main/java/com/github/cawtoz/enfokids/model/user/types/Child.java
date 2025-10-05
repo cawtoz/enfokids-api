84d929debe53c0ed1676a07e387434e05b219da6
cawtoz <cawtoz.dev@gmail.com>
2025-10-04 20:57:59 -0500

    add caregiver-children relationship to Caregiver and Child models

--- src/main/java/com/github/cawtoz/enfokids/model/user/types/Child.java
+++ src/main/java/com/github/cawtoz/enfokids/model/user/types/Child.java
@@ -1,12 +1,20 @@
 package com.github.cawtoz.enfokids.model.user.types;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.github.cawtoz.enfokids.model.relation.CaregiverChild;
 import com.github.cawtoz.enfokids.model.user.User;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "children")
 public class Child extends User {
+
+    @OneToMany(mappedBy = "child")
+    private Set<CaregiverChild> caregiverChildren = new HashSet<>();
     
 }
