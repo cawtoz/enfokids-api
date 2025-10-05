e8a1f7d1181d7be4decdfeb99d735570a1f99f51
cawtoz <cawtoz.dev@gmail.com>
2025-10-04 22:13:52 -0500

    add Activity, Assignment, and Progress models

---
+++ src/main/java/com/github/cawtoz/enfokids/model/activity/Activity.java
@@ -0,0 +1,37 @@
+package com.github.cawtoz.enfokids.model.activity;
+
+import com.github.cawtoz.enfokids.model.activity.enums.ActivityTypeEnum;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "activities")
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 3000)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 30, nullable = false)
+    private ActivityTypeEnum type;
+
+    private String image_url;
+
+    private String resource_url;
+
+}
+
