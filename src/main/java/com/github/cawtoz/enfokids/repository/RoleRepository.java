c8aa4fa0f6a3dbe3c4dc8c12efc138d3dd097f4b
cawtoz <cawtoz.dev@gmail.com>
2025-10-10 10:36:14 -0500

    add findByName method to RoleRepository

--- src/main/java/com/github/cawtoz/enfokids/repository/RoleRepository.java
+++ src/main/java/com/github/cawtoz/enfokids/repository/RoleRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.github.cawtoz.enfokids.model.role.Role;
+import com.github.cawtoz.enfokids.model.role.RoleEnum;
+
+import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    
+    Optional<Role> findByName(RoleEnum name);
 }
