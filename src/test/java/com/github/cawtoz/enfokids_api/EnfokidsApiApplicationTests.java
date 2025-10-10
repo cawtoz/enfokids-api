328a0a65d1e47547b2edc2a274e840cd25a3774a
cawtoz <cawtoz.dev@gmail.com>
2025-10-10 10:59:26 -0500

    add Spring Boot test context loading

---
+++ src/test/java/com/github/cawtoz/enfokids_api/EnfokidsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cawtoz.enfokids_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnfokidsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
