e7320c3bc8b7b72efa87f1b7877c5804759e2cb8
cawtoz <cawtoz.dev@gmail.com>
2025-10-10 22:26:29 -0500

    add Lombok annotations to Therapist model

--- src/main/java/com/github/cawtoz/enfokids/model/user/types/Therapist.java
+++ src/main/java/com/github/cawtoz/enfokids/model/user/types/Therapist.java
@@ -5,9 +5,13 @@ import com.github.cawtoz.enfokids.model.user.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Entity
 @Table(name = "therapists")
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class Therapist extends User {
 
     @Column(length = 30, nullable = false)
